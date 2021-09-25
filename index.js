71d64e682531478f23f53567b400a1c6b678fa62
HM <hieumdd@gmail.com>
2021-09-25 11:54:35 +0700

    !feat: Added mongoose, Removed mongodb

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
 require('dotenv').config();
 const axios = require('axios');
-const { MongoClient } = require('mongodb');
+const mongoose = require('mongoose');
 
-const { fbAdsSchema, FacebookAdsInsights } = require('./models');
+const { FacebookAdsInsights, keys, fields } = require('./models');
 
 const API_VER = 'v12.0';
 axios.defaults.baseURL = `https://graph.facebook.com/${API_VER}`;
 
-const mongoClient = new MongoClient(process.env.MONGO_URI);
-
 const getReportId = async (adAccountId, attempt = 0) => {
   try {
-    return pollReport(sendReportRequest(adAccountId));
+    return pollReport(await sendReportRequest(adAccountId));
   } catch (err) {
     console.log(err.message, attempt);
     return await getReportId(adAccountId, attempt + 1);
@@ -21,10 +19,9 @@ const getReportId = async (adAccountId, attempt = 0) => {
 const sendReportRequest = async (adAccountId) => {
   const { data } = await axios.post(`/${adAccountId}/insights`, {
     access_token: process.env.ACCESS_TOKEN,
-    fields: Object.entries(fbAdsSchema.obj)
-      .map((i) => i[0])
-      .join(','),
+    fields,
     level: 'ad',
+    time_range: JSON.stringify({ since: '2021-09-01', until: '2021-09-05' }),
     time_increment: 1,
   });
   return data.report_run_id;
@@ -67,87 +64,22 @@ const getAdsInsights = async (adAccountId) => {
 };
 
 const loadMongo = async (data) => {
-  await mongoClient.connect();
-  const actionTypeMap = ({ action_type, value }) => ({
-    action_type,
-    value: Number(value),
-  });
+  await mongoose.connect(process.env.MONGO_URI);
   const bulkUpdateOps = data.map((item) => ({
     updateOne: {
-      filter: {
-        date_start: new Date(item.date_start),
-        date_stop: new Date(item.date_stop),
-        account_id: parseInt(item.account_id),
-        account_name: item.account_name,
-        campaign_id: parseInt(item.campaign_id),
-        campaign_name: item.campaign_name,
-        adset_id: parseInt(item.adset_id),
-        adset_name: item.adset_name,
-        ad_id: parseInt(item.ad_id),
-        ad_name: item.ad_name,
-      },
-      update: {
-        $set: {
-          date_start: new Date(item.date_start),
-          date_stop: new Date(item.date_stop),
-          account_id: Number(item.account_id),
-          account_name: item.account_name,
-          campaign_id: Number(item.campaign_id),
-          campaign_name: item.campaign_name,
-          adset_id: Number(item.adset_id),
-          adset_name: item.adset_name,
-          ad_id: Number(item.ad_id),
-          ad_name: item.ad_name,
-          account_currency: item.account_currency,
-          actions: item.actions.map(actionTypeMap),
-          action_values: item.action_values.map(actionTypeMap),
-          clicks: Number(item.clicks),
-          conversion_rate_ranking: item.conversion_rate_ranking,
-          conversion_values: item.conversion_values.map(actionTypeMap),
-          conversions: item.conversion_values.map(actionTypeMap),
-          cost_per_action_type: item.cost_per_action_type.map(actionTypeMap),
-          cost_per_conversion: item.cost_per_conversion.map(actionTypeMap),
-          cost_per_unique_action_type:
-            item.cost_per_unique_action_type.map(actionTypeMap),
-          cost_per_unique_click: Number(item.cost_per_unique_click),
-          cpc: Number(item.cpc),
-          cpm: Number(item.cpm),
-          ctr: Number(item.ctr),
-          engagement_rate_ranking: item.engagement_rate_ranking,
-          frequency: Number(item.frequency),
-          impressions: Number(item.impressions),
-          inline_link_click_ctr: Number(item.inline_link_click_ctr),
-          inline_link_clicks: Number(item.inline_link_clicks),
-          objective: item.objective,
-          optimization_goal: item.optimization_goal,
-          quality_ranking: item.quality_ranking,
-          reach: Number(item.reach),
-          spend: Number(item.spend),
-          unique_actions: item.unique_actions.map(actionTypeMap),
-          unique_clicks: Number(item.unique_clicks),
-          unique_ctr: Number(item.unique_ctr),
-          unique_link_clicks_ctr: Number(item.unique_link_clicks_ctr),
-          video_30_sec_watched_actions:
-            item.video_30_sec_watched_actions.map(actionTypeMap),
-          video_p100_watched_actions:
-            item.video_p100_watched_actions.map(actionTypeMap),
-          video_p25_watched_actions:
-            item.video_p25_watched_actions.map(actionTypeMap),
-          video_p50_watched_actions:
-            item.video_p50_watched_actions.map(actionTypeMap),
-          video_p75_watched_actions:
-            item.video_p75_watched_actions.map(actionTypeMap),
-          video_p95_watched_actions:
-            item.video_p95_watched_actions.map(actionTypeMap),
-        },
-      },
+      filter: Object.fromEntries(keys.map((i) => [i, item[i]])),
+      update: Object.fromEntries(fields.map((i) => [i, item[i]])),
       upsert: true,
     },
   }));
-  const collection = mongoClient.db('dev').collection('fb_ads_raw');
-  const { modifiedCount } = await collection.bulkWrite(bulkUpdateOps);
-  await mongoClient.close();
-  return modifiedCount;
+  try {
+    const { modifiedCount } = await FacebookAdsInsights.bulkWrite(
+      bulkUpdateOps
+    );
+    return modifiedCount;
+  } catch (err) {
+    console.log(err.message);
+  }
 };
 
 const main = async () => {
@@ -155,6 +87,7 @@ const main = async () => {
   const data = await Promise.all(
     adAccountIds.map(async (adAccountId) => await getAdsInsights(adAccountId))
   );
+
   const modifiedCount = await loadMongo(data.flat());
   console.log({ modifedCount: modifiedCount });
 };
--- models.js
+++ models.js
@@ -7,57 +7,70 @@ const actionBreakdowns = {
   value: Number,
 };
 
-export const fbAdsSchema = new Schema({
-  date_start: { type: Date, required: true },
-  date_stop: { type: Date, required: true },
-  account_id: { type: Number, required: true },
-  campaign_id: { type: Number, required: true },
-  adset_id: { type: Number, required: true },
-  ad_id: { type: Number, required: true },
-  account_name: String,
-  campaign_name: String,
-  adset_name: String,
-  ad_name: String,
-  account_currency: String,
-  actions: actionBreakdowns,
-  action_values: actionBreakdowns,
-  clicks: Number,
-  conversion_rate_ranking: String,
-  conversion_values: actionBreakdowns,
-  conversions: actionBreakdowns,
-  cost_per_action_type: actionBreakdowns,
-  cost_per_conversion: actionBreakdowns,
-  cost_per_unique_action_type: actionBreakdowns,
-  cost_per_unique_click: Number,
-  cpc: Number,
-  cpm: Number,
-  ctr: Number,
-  engagement_rate_ranking: String,
-  frequency: Number,
-  impressions: Number,
-  inline_link_click_ctr: Number,
-  inline_link_clicks: Number,
-  objective: String,
-  optimization_goal: String,
-  quality_ranking: String,
-  reach: Number,
-  spend: Number,
-  unique_actions: actionBreakdowns,
-  unique_clicks: Number,
-  unique_ctr: Number,
-  unique_link_clicks_ctr: Number,
-  video_30_sec_watched_actions: actionBreakdowns,
-  video_p100_watched_actions: actionBreakdowns,
-  video_p25_watched_actions: actionBreakdowns,
-  video_p50_watched_actions: actionBreakdowns,
-  video_p75_watched_actions: actionBreakdowns,
-  video_p95_watched_actions: actionBreakdowns,
-});
+const fbAdsSchema = new Schema(
+  {
+    date_start: { type: Date, required: true },
+    date_stop: { type: Date, required: true },
+    account_id: { type: Number, required: true },
+    campaign_id: { type: Number, required: true },
+    adset_id: { type: Number, required: true },
+    ad_id: { type: Number, required: true },
+    account_name: String,
+    campaign_name: String,
+    adset_name: String,
+    ad_name: String,
+    account_currency: String,
+    actions: actionBreakdowns,
+    action_values: actionBreakdowns,
+    clicks: Number,
+    conversion_rate_ranking: String,
+    conversion_values: actionBreakdowns,
+    conversions: actionBreakdowns,
+    cost_per_action_type: actionBreakdowns,
+    cost_per_conversion: actionBreakdowns,
+    cost_per_unique_action_type: actionBreakdowns,
+    cost_per_unique_click: Number,
+    cpc: Number,
+    cpm: Number,
+    ctr: Number,
+    engagement_rate_ranking: String,
+    frequency: Number,
+    impressions: Number,
+    inline_link_click_ctr: Number,
+    inline_link_clicks: Number,
+    objective: String,
+    optimization_goal: String,
+    quality_ranking: String,
+    reach: Number,
+    spend: Number,
+    unique_actions: actionBreakdowns,
+    unique_clicks: Number,
+    unique_ctr: Number,
+    unique_link_clicks_ctr: Number,
+    video_30_sec_watched_actions: actionBreakdowns,
+    video_p100_watched_actions: actionBreakdowns,
+    video_p25_watched_actions: actionBreakdowns,
+    video_p50_watched_actions: actionBreakdowns,
+    video_p75_watched_actions: actionBreakdowns,
+    video_p95_watched_actions: actionBreakdowns,
+  },
+  { collection: 'fb_ads_raw' }
+);
 
-export const FacebookAdsInsights = mongoose.model(
+const FacebookAdsInsights = mongoose.model(
   'FacebookAdsInsightsRaw',
   fbAdsSchema
 );
 
-const x = Object.entries(fbAdsSchema.obj).filter((i) => i[1].required);
-x;
+const keys = Object.entries(fbAdsSchema.obj)
+  .filter(([_, type]) => type.required)
+  .map(([name, _]) => name);
+
+const fields = Object.entries(fbAdsSchema.obj).map(([name, _]) => name);
+
+module.exports = {
+  fbAdsSchema,
+  FacebookAdsInsights,
+  keys,
+  fields,
+};
