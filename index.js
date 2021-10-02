0ba3686ce66c0f28cfadfdb10d8c648e07bb09a1
HM <hieumdd@gmail.com>
2021-10-02 21:35:22 +0700

    feat: Updated index.js to handle API fetch

--- index.js
+++ index.js
@@ -1,126 +1,67 @@
 require('dotenv').config();
-const dayjs = require('dayjs');
+const fs = require('fs/promises');
 const axios = require('axios');
-const mongoose = require('mongoose');
 
-const { FacebookAdsInsights, keys, fields } = require('./models');
+const { getAdsInsights } = require('./services/Facebook');
+const { loadMongo } = require('./services/Mongo');
+const { FacebookAdsInsights, keys, fields } = require('./models/models');
 
-const API_VER = 'v12.0';
-axios.defaults.baseURL = `https://graph.facebook.com/${API_VER}`;
+const instance = axios.create({
+  baseURL: 'https://abc.1gu.xyz',
+  headers: {
+    key: process.env.API_KEY,
+  },
+});
 
-const sendReportRequest = async (adAccountId, start, end) => {
-  const { data } = await axios.post(`/${adAccountId}/insights`, {
-    access_token: process.env.ACCESS_TOKEN,
-    fields: fields.join(','),
-    filter: JSON.stringify([
-      { field: 'ad.impressions', operator: 'GREATER_THAN', value: 0 },
-      {
-        field: 'ad.effective_status',
-        operator: 'IN',
-        value: [
-          'ACTIVE',
-          'PAUSED',
-          'DELETED',
-          'PENDING_REVIEW',
-          'DISAPPROVED',
-          'PREAPPROVED',
-          'PENDING_BILLING_INFO',
-          'CAMPAIGN_PAUSED',
-          'ARCHIVED',
-          'ADSET_PAUSED',
-          'IN_PROCESS',
-          'WITH_ISSUES',
-        ],
-      },
-    ]),
-    level: 'campaign',
-    time_range: JSON.stringify({ since: start, until: end }),
-    time_increment: 1,
-  });
-  return data.report_run_id;
-};
-
-const pollReport = async (reportId) => {
-  const { data } = await axios.get(`/${reportId}`, {
-    params: { access_token: process.env.ACCESS_TOKEN },
-  });
-  return data.async_percent_completion === 100
-    ? reportId
-    : new Promise((resolve) =>
-        setTimeout(() => {
-          setTimeout(() => resolve(pollReport(reportId)));
-        }, 10000)
-      );
-};
-
-const getReportId = async (adAccountId, start, end, attempt = 0) => {
-  try {
-    return pollReport(await sendReportRequest(adAccountId, start, end));
-  } catch (err) {
-    console.log(err.message, attempt);
-    return getReportId(adAccountId, start, end, attempt + 1);
-  }
-};
-
-const getData = async (reportId, after = null) => {
-  const params = {
-    access_token: process.env.ACCESS_TOKEN,
-    limit: 500,
-  };
-  if (after) {
-    params.after = after;
-  }
-  const {
-    data: { data, paging },
-  } = await axios.get(`/${reportId}/insights`, {
-    params,
-  });
-  const { next } = paging;
-  return next ? [...data, await getData(reportId, next)] : [...data];
-};
-
-const getAdsInsights = async (adAccountId, start = null, end = null) => {
-  const [_start, _end] =
-    [start, end] ??
-    [dayjs(), dayjs().subtract(7, 'day')].map((i) => i.format('YYYY-MM-DD'));
-  const reportId = await pollReport(
-    await getReportId(adAccountId, _start, _end)
-  );
-  const data = await getData(reportId);
-  return data;
-};
-
-const loadMongo = async (data) => {
-  await mongoose.connect(process.env.MONGO_URI);
-  const bulkUpdateOps = data.map((item) => ({
-    updateOne: {
-      filter: Object.fromEntries(keys.map((i) => [i, item[i]])),
-      update: Object.fromEntries(fields.map((i) => [i, item[i]])),
-      upsert: true,
-    },
+const getAdAccounts = async () => {
+  const { data } = await instance.get('/adAccounts');
+  return Object.entries(data).map(([key, value]) => ({
+    adAccount: key,
+    ids: value.map((i) => i.trim()).map((i) => (i ? i : null)),
   }));
-  let modifiedCount = null;
-  try {
-    ({ modifiedCount } = await FacebookAdsInsights.bulkWrite(bulkUpdateOps));
-  } catch (err) {
-    console.log(err.message);
-  } finally {
-    mongoose.disconnect();
-  }
-  return modifiedCount;
+};
+
+const getEvents = async () => {
+  const { data } = await instance.get('/events');
+  return data.map((i) => ({
+    campaignId: i['Campaign ID'],
+    nonProfit: i['Nonprofit'],
+    start: i['Live Date'],
+    end: i['Start Date'],
+  }));
 };
 
 const main = async () => {
-  const [start, end] = ['2021-09-01', '2021-09-05'];
-  const adAccountIds = ['act_3921338037921594'];
-  const data = await Promise.all(
-    adAccountIds.map(async (adAccountId) =>
-      getAdsInsights(adAccountId, start, end)
+  const events = await getEvents();
+  const adAccounts = await getAdAccounts();
+  const eventsWithAdAccount = events
+    .map((event) => {
+      const mappedAdAccount = adAccounts.find(
+        (adAccount) => adAccount.adAccount === event.nonProfit
+      );
+      return mappedAdAccount.ids.map((id) => ({
+        ...event,
+        adAccountId: id,
+      }));
+    })
+    .flat();
+  const results = await (
+    await Promise.all(
+      eventsWithAdAccount.map(async (i) => await getAdsInsights(i))
     )
+  ).flat();
+  const data = results.filter((i) => !i['err']);
+  const err = results.filter((i) => i['err']);
+  const loadResults = await loadMongo(
+    { model: FacebookAdsInsights, keys, fields },
+    data
   );
-
-  const modifiedCount = await loadMongo(data.flat());
-  console.log({ modifiedCount });
+  await fs.writeFile('exports/errors.json', JSON.stringify(err));
+  return {
+    err,
+    loadResults,
+  };
 };
 
-main();
+const run = main();
+console.log(run);
