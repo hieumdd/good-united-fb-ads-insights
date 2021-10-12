e84f375a3787f272b9c673b00c065b6271c933cf
HM <hieumdd@gmail.com>
2021-10-12 17:32:48 +0700

    feat: Added chunk loading for Mongo

--- services/Mongo.js
+++ services/Mongo.js
@@ -1,26 +1,41 @@
 const mongoose = require('mongoose');
+const { chunk, sumBy } = require('lodash');
 
 const loadMongo = async ({ model, keys, fields }, data) => {
-  const bulkUpdateOps = data.map((item) => ({
-    updateOne: {
-      filter: Object.fromEntries(keys.map((i) => [i, item[i]])),
-      update: Object.fromEntries(fields.map((i) => [i, item[i]])),
-      upsert: true,
-    },
-  }));
+  const dataChunks = chunk(data, 100);
+  const bulkUpdateOps = dataChunks.map((dataChunk) =>
+    dataChunk.map((item) => ({
+      updateOne: {
+        filter: Object.fromEntries(keys.map((i) => [i, item[i]])),
+        update: Object.fromEntries(fields.map((i) => [i, item[i]])),
+        upsert: true,
+      },
+    }))
+  );
   try {
     await mongoose.connect(process.env.MONGO_URI);
     console.log('Loading to Mongo...');
-    const { deletedCount, insertedCount, modifiedCount, upsertedCount } =
-      await model.bulkWrite(bulkUpdateOps, { ordered: false });
+    const results = await Promise.all(
+      bulkUpdateOps.map(async (op, i) => {
+        console.log(`Loading chunk ${i}`);
+        const { deletedCount, insertedCount, modifiedCount, upsertedCount } =
+          await model.bulkWrite(op, { w: 0, ordered: false });
+        console.log(`Loaded chunk ${i}`);
+        return {
+          deletedCount,
+          insertedCount,
+          modifiedCount,
+          upsertedCount,
+        };
+      })
+    );
     return [
       null,
-      {
-        deletedCount,
-        insertedCount,
-        modifiedCount,
-        upsertedCount,
-      },
+      Object.fromEntries(
+        ['deletedCount', 'insertedCount', 'modifiedCount', 'upsertedCount'].map(
+          (i) => [i, sumBy(results, (j) => j.deletedCount)]
+        )
+      ),
     ];
   } catch (err) {
     return [err, null];
