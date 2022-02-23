3b19a1021eadd15b40e1d8385e7ae08257c90928
HM <hieumdd@gmail.com>
2022-02-23 10:08:49 +0700

    feat: Added test

---
+++ test/testUnit.ts
@@ -0,0 +1,11 @@
+import { pipelineService } from '../src/facebook/facebookService';
+import { eventService, taskService } from '../src/goodUnited/goodUnitedService';
+import { assert } from 'chai';
+
+describe('Facebook', () => {
+    it('Pipeline Service', () => {
+        const options = {
+            adAccountId: 
+        }; 
+    });
+});
