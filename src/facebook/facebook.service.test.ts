b72972cbfcd74c613e48cf68ad1ad8488af4bb2c
HM <hieumdd@gmail.com>
2023-01-11 19:39:13 +0700

    feat: Add FBAdsAgeGenderAd

--- src/facebook/facebook.service.test.ts
+++ src/facebook/facebook.service.test.ts
@@ -1,17 +1,17 @@
 import { pipelineService } from './facebook.service';
-import { Pipeline, CampaignInsights, AgeGenderInsights } from './pipeline.const';
-
-const cases = [
-    // [CampaignInsights.collection, CampaignInsights],
-    [AgeGenderInsights.collection, AgeGenderInsights],
-] as [string, Pipeline][];
+import { CampaignInsights, AgeGenderInsights, AgeGenderAdInsights } from './pipeline.const';
 
 describe('Facebook Service', () => {
-    it.each(cases)('Pipeline Service %p', async (_, pipeline) => {
-        const options = {
-            accountId: '137172389777147',
-        };
+    it.each([CampaignInsights, AgeGenderInsights, AgeGenderAdInsights])(
+        'Pipeline Service %p',
+        async (pipeline) => {
+            const options = {
+                accountId: '137172389777147',
+                start: '2022-12-01',
+                end: '2023-01-01',
+            };
 
-        return pipelineService(options, pipeline).then((res) => expect(res).toBeTruthy());
-    });
+            return pipelineService(options, pipeline).then((res) => expect(res).toBeTruthy());
+        },
+    );
 });
